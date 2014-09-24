a7a27906c0f3722b8a0df6ed061eeab8b33cf257
Patrick LEFEVRE <patrick.lefevre@gmail.com>
2014-09-24 22:41:39 +0200

    Update global.js
    Browsers detection if needed.

--- js/global.js
+++ js/global.js
@@ -1,3 +1,6 @@
 /*!
 
 */
+function get_browser(){var e,r=navigator.userAgent,a=r.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i)||[];return/trident/i.test(a[1])?(e=/\brv[ :]+(\d+)/g.exec(r)||[],"IE "+(e[1]||"")):"Chrome"===a[1]&&(e=r.match(/\bOPR\/(\d+)/),null!=e)?"Opera "+e[1]:(a=a[2]?[a[1],a[2]]:[navigator.appName,navigator.appVersion,"-?"],null!=(e=r.match(/version\/(\d+)/i))&&a.splice(1,1,e[1]),a[0])}function get_browser_version(){var e,r=navigator.userAgent,a=r.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i)||[];return/trident/i.test(a[1])?(e=/\brv[ :]+(\d+)/g.exec(r)||[],"IE "+(e[1]||"")):"Chrome"===a[1]&&(e=r.match(/\bOPR\/(\d+)/),null!=e)?"Opera "+e[1]:(a=a[2]?[a[1],a[2]]:[navigator.appName,navigator.appVersion,"-?"],null!=(e=r.match(/version\/(\d+)/i))&&a.splice(1,1,e[1]),a[1])};
+var browser=get_browser();
+var browser_version=get_browser_version();
